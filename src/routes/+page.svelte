653da77e44eda645bc8d2b0219c259669eab1b0a
Skyrdow <lucas.mesias@usach.cl>
2023-12-19 16:15:09 -0300

    tailwind + pruebas con api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
+<h1 class="w-52">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#await pokePromise then games }
+    {#each games as game}
+        <h1>{game.version.name}</h1>
+    {/each}
+{/await}
+
+<script>
+    import axios from "axios";
+    const pokePromise = axios.get("https://pokeapi.co/api/v2/pokemon/ditto")
+    .then((response) => { 
+        return response.data.game_indices});
+</script>
