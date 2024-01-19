a7dad4a844f04ae08e127de7c3323241c40fbb70
Skyrdow <lucas.mesias@usach.cl>
2024-01-19 19:16:38 -0300

    add svg background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,14 @@
   import "../app.css";
 </script>
 
-<header class="bg-gray-300 p-4 mb-10">
+<header class="bg-gradient-to-b from-cyan-500 to-blue-500 p-4 h-[5vh]">
   <h1>Lucas Mesias</h1>
 </header>
-<main class="min-h-[90vh]">
+<main class="min-h-[90vh] svg-bg">
   <slot />
 </main>
-<footer>
-  <a href="https://icons8.com/icon/12908/estación-de-trabajo"
-    >Estación de trabajo</a
+<footer class="bg-gradient-to-b from-cyan-500 to-blue-500 h-[5vh]">
+  <a href="https://www.svgbackgrounds.com/"
+    >Background CSS by svgbackgrounds.com</a
   >
-  icon by <a href="https://icons8.com">Icons8</a>
 </footer>
--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -24,7 +24,18 @@
   }
 
   function add_ramo() {
-    data.push({ nombre: "Nuevo Ramo", evaluaciones: [] });
+    data.push({ nombre: "Nuevo Ramo", evaluaciones: [{
+          nombre: "",
+          nota: 1,
+          ponderacion: 0.5,
+          es_pendiente: false,
+        }, {
+          nombre: "",
+          nota: 1,
+          ponderacion: 0.5,
+          es_pendiente: false,
+        }] });
+    current_index = data.length - 1;
     data = data;
   }
   /** @param {Number} index*/
@@ -37,21 +48,30 @@
 <div class="bg-slate-400 p-1 w-fit rounded-lg flex flex-row gap-1 items-center">
   {#each data as ramo, i (i)}
     <div class="bg-slate-100 p-1 w-fit rounded-lg flex flex-row gap-1">
-      <button class="btn btn-outline btn-xs h-6" on:click={() => {current_index = i;}}>{ramo.nombre}</button>
-      <button class="btn btn-square btn-outline btn-xs h-6" on:click={() => {remove_ramo(i);}}>
-        <img src="/cross.svg" alt="Delete icon">
+      <button
+        class="btn btn-outline btn-xs h-6"
+        on:click={() => {
+          current_index = i;
+        }}>{ramo.nombre}</button
+      >
+      <button
+        class="btn btn-square btn-outline btn-xs h-6"
+        on:click={() => {
+          remove_ramo(i);
+        }}
+      >
+        <img src="/cross.svg" alt="Delete icon" />
       </button>
     </div>
   {/each}
   <button class="btn btn-square btn-outline btn-xs h-6" on:click={add_ramo}>
-    <img src="/plus.svg" alt="Add icon">
+    <img src="/plus.svg" alt="Add icon" />
   </button>
 </div>
 {#key current_index}
-  
-<div class="bg-primary">
-  <Ramo ramo={data[current_index]} />
-</div>
+  <div class="bg-primary">
+    <Ramo ramo={data[current_index]} />
+  </div>
 {/key}
 
 <svelte:window on:keydown={onKeyDown} />
