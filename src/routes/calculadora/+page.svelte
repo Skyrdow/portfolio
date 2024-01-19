cd47665b072a9007a067ce4dd2c6103dd136dccc
Skyrdow <lucas.mesias@usach.cl>
2024-01-19 14:46:56 -0300

    add ponderacion checker

--- src/routes/calculadora/+page.svelte
+++ src/routes/calculadora/+page.svelte
@@ -1,22 +1,20 @@
 <script>
-  import Ramo from './Ramo.svelte';
+  import Ramo from "./Ramo.svelte";
   let str_data = null;
 
-  if (typeof localStorage !== 'undefined') {
+  if (typeof localStorage !== "undefined") {
     str_data = localStorage.getItem("calc");
   }
-  
-  /** @type {Array<import('./calculadora').Ramo>} */
-  let data = [{nombre: 'Nuevo Ramo', evaluaciones: []}];
-  console.log(str_data);
-  if (str_data !== null)
-    data = JSON.parse(str_data);
 
+  /** @type {Array<import('./calculadora').Ramo>} */
+  let data = [{ nombre: "Nuevo Ramo", evaluaciones: [] }];
+  console.log(str_data);
+  if (str_data !== null) data = JSON.parse(str_data);
 </script>
 
 {#each data as ramo}
   <div class="bg-primary">
     <h1>{ramo.nombre}</h1>
-    <Ramo evaluaciones={ramo.evaluaciones}/>
+    <Ramo evaluaciones={ramo.evaluaciones} />
   </div>
 {/each}
